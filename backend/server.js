843f22e8da7f33a035a4efb86694d9a0f7d48e24
Sayjan J Singh <sayjan7777@gmail.com>
2025-04-16 20:53:38 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+//backend file
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
@@ -6,28 +7,29 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/api/data") {
-    // Set CORS headers
+    //set CORS headers since frontend(React) works on different port than backend
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-    const filePath = path.join(__dirname, "data.json");
-
-    fs.readFile(filePath, "utf8", (err, jsonData) => {
+    //read the file, __dirname sets the absolute path of the directory
+    fs.readFile(path.join(__dirname, "data.json"), "utf8", (err, data) => {
       if (err) {
         res.writeHead(500, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Failed to read file" }));
+        res.end(JSON.stringify({ error: "Failed to read data file." }));
       } else {
+        //return the file data in response in json format
         res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(jsonData);
+        res.end(data);
       }
     });
   } else {
+    //return not found for any other path requested.
     res.writeHead(404);
     res.end("Not Found");
   }
 });
 
+//start the server and log the URL
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,46 @@
+// main file for rendering charts and tables
 import { useEffect, useState } from "react";
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography, CircularProgress } from "@mui/material";
 import DChart from "./components/DChart";
 import DTable from "./components/DTable";
 
 function App() {
   const [data, setData] = useState(null);
 
+  //fetch the data from the backend API
   useEffect(() => {
-    fetch("http://localhost:3000/api/data")
+    fetch("http://localhost:3000/api/data") //url set on the backend
       .then((res) => res.json())
       .then(setData)
       .catch((err) => console.error("Error fetching data:", err));
-  }, []);
+  }, []); // [] makes sure data is fetched once when the component loads
 
-  if (!data) return <Typography>NO DATA FOUND</Typography>;
+  if (!data)
+    return (
+      <Container style={{ textAlign: "center", marginTop: 50 }}>
+        <CircularProgress />
+      </Container>
+    ); //if data hasn't been fetched yet, show loading effect
 
+  //calculate the table data for count
   const CountTable = (data) =>
-    data.map((stage, index) => {
-      const nextStage = data[index + 1];
-      const moved =
-        nextStage?.count ?? (stage.label === "Won" ? stage.count : 0);
-      const lost = stage.count - moved;
+    //format the raw data into table-ready structure
+
+    data.map((curr, index) => {
+      const nxt = data[index + 1];
+      const moved = nxt?.count ?? (curr.label === "Won" ? curr.count : 0);
+      // Ones who didn't move to the next stage are considered lost
+
+      const lost = curr.count - moved;
+      //win rate is the percentage of last count/ current count
       const winRate = (
-        (data[data.length - 1].count / stage.count) *
+        (data[data.length - 1].count / curr.count) *
         100
       ).toFixed(0);
-      const diffRate = stage.diffRate;
+      const diffRate = curr.diffRate;
       return {
-        stage: stage.label,
-        cameToStage: stage.count,
+        stage: curr.label,
+        cameToStage: curr.count,
         lost,
         moved,
         winRate: `${winRate}%`,
@@ -36,19 +48,23 @@ function App() {
       };
     });
 
+  //calculate the table data for acv
   const AcvTable = (data) =>
-    data.map((stage, index) => {
-      const nextStage = data[index + 1];
-      const moved = nextStage?.acv ?? (stage.label === "Won" ? stage.acv : 0);
-      const lost = stage.acv - moved;
-      const winRate = ((data[data.length - 1].acv / stage.acv) * 100).toFixed(
-        0
-      );
-      const diffacvRate = stage.diffacvRate;
+    //format the raw data into table-ready structure
+
+    data.map((curr, index) => {
+      const nxt = data[index + 1];
+      const moved = nxt?.acv ?? (curr.label === "Won" ? curr.acv : 0);
+
+      const lost = curr.acv - moved; //Ones who didn't move to the next stage are considered lost
+
+      //win rate is the percentage of last avc/ current acv
+      const winRate = ((data[data.length - 1].acv / curr.acv) * 100).toFixed(0);
+      const diffacvRate = curr.diffacvRate;
 
       return {
-        stage: stage.label,
-        cameToStage: stage.acv,
+        stage: curr.label,
+        cameToStage: curr.acv,
         lost,
         moved,
         winRate: `${winRate}%`,
@@ -56,41 +72,40 @@ function App() {
       };
     });
 
+  //
   const tableDataCount = CountTable(data);
   const tableDataAcv = AcvTable(data);
+
+  //calculate total lost opportunities (excluding 'Won')
 
   const totalLostCount = tableDataCount.reduce(
     (sum, row) => sum + (row.stage === "Won" ? 0 : row.lost),
     0
   );
-
   const totalLostAcv = tableDataAcv.reduce(
     (sum, row) => sum + (row.stage === "Won" ? 0 : row.lost),
     0
   );
 
   return (
+    //equivalent to div in html
     <Container style={{ padding: 10, marginTop: 30 }}>
       <Grid container spacing={4}>
         <Grid size={{ xs: 12, sm: 6 }}>
+          {" "}
+          {/* xs,sm used for making it responsive, 12 stands for full width and 6 for half, meaning it would take full width available if the screen size is less than 600 otherwise it will take half the screen. */}
           <DChart data={data} type="count" />
+          {/* component for funnel chart passed with a type prop tell chart what to visualize (count or acv) */}
         </Grid>
         <Grid size={{ xs: 12, sm: 6 }}>
           <DChart data={data} type="acv" />
         </Grid>
         <Grid size={{ xs: 12, sm: 6 }}>
-          <DTable
-            data={tableDataCount}
-            title="Funnel Table (Count)"
-            totalLost={totalLostCount}
-          />
+          <DTable data={tableDataCount} totalLost={totalLostCount} />
+          {/* component for Table passed with multiple arguments which is the table data and total lost count */}
         </Grid>
         <Grid size={{ xs: 12, sm: 6 }}>
-          <DTable
-            data={tableDataAcv}
-            title="Funnel Table (ACV)"
-            totalLost={totalLostAcv}
-          />
+          <DTable data={tableDataAcv} totalLost={totalLostAcv} />
         </Grid>
       </Grid>
     </Container>
